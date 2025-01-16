bc44fe99474c42c42847b961d6833b4adf3a82ab
jho7535 <jho7535@naver.com>
2025-01-16 15:53:38 +0900

    solved on 25.01.16

--- programmers/귤_고르기.java
+++ programmers/귤_고르기.java
@@ -1,4 +1,31 @@
 package nestnet_algorithm_2023_2.JeongHanUl.programmers;
 
+import java.util.*;
+
 public class 귤_고르기 {
+    class Solution {
+        public int solution(int k, int[] tangerine) {
+            Integer[] arr = new Integer[10000001];
+            Arrays.fill(arr, 0);
+            for (int i : tangerine) {
+                arr[i]++;
+            }
+
+            Arrays.sort(arr, (a, b) -> b - a);
+
+            int index = 0, cnt = 1;
+            while (k > 0 && index < 10000001) {
+                if (arr[index] == 0) {
+                    index++;
+                    cnt++;
+                    continue;
+                }
+
+                arr[index]--;
+                k--;
+            }
+
+            return cnt;
+        }
+    }
 }
