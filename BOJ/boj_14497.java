25200b7ecfc6afb9a9abe5288aee9746cc10e10d
jho7535 <jho7535@naver.com>
2025-03-24 23:43:25 +0900

    Optimize using 0-1 BFS

--- BOJ/boj_14497.java
+++ BOJ/boj_14497.java
@@ -15,66 +15,42 @@ public class boj_14497 {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         char[][] map = new char[n][];
+        int[][] cnt = new int[n][m];
+        for (int i = 0; i < n; i++) Arrays.fill(cnt[i], Integer.MAX_VALUE);
 
         st = new StringTokenizer(br.readLine());
         int r1 = Integer.parseInt(st.nextToken()) - 1;
         int c1 = Integer.parseInt(st.nextToken()) - 1;
         int r2 = Integer.parseInt(st.nextToken()) - 1;
         int c2 = Integer.parseInt(st.nextToken()) - 1;
+        cnt[r1][c1] = 0;
 
         for (int i = 0; i < n; i++) map[i] = br.readLine().toCharArray();
 
-//        for (int i = 0; i < n; i++) {
-//            for (int j = 0; j < m; j++) System.out.print(map[i][j] + " ");
-//            System.out.println();
-//        }
+        Deque<int[]> deque = new ArrayDeque<>();
+        deque.addFirst(new int[]{r1, c1});
+        boolean[][] visited = new boolean[n][m];
+        visited[r1][c1] = true;
+        while (!deque.isEmpty()) {
+            int[] current = deque.poll();
+            int curRow = current[0];
+            int curCol = current[1];
 
-        int cnt = 0;
-        boolean flag = true;
-        while (flag) {
-            cnt++;
-            Queue<int[]> queue = new LinkedList<>();
-            queue.add(new int[]{r1, c1});
-            boolean[][] visited = new boolean[n][m];
-            visited[r1][c1] = true;
-            Queue<int[]> deleteQueue = new LinkedList<>();
+            for (int i = 0; i < 4; i++) {
+                int nr = curRow + dr[i];
+                int nc = curCol + dc[i];
 
-            while (!queue.isEmpty() && flag) {
-                int[] current = queue.poll();
-                int curRow = current[0];
-                int curCol = current[1];
+                if (nr < 0 || nr >= n || nc < 0 || nc >= m) continue;
+                if (visited[nr][nc]) continue;
 
-                for (int i = 0; i < 4; i++) {
-                    int nr = curRow + dr[i];
-                    int nc = curCol + dc[i];
-
-                    if (nr < 0 || nr >= n || nc < 0 || nc >= m) continue;
-                    if (visited[nr][nc]) continue;
-                    if (map[nr][nc] == '1') {
-                        deleteQueue.add(new int[]{nr, nc});
-                        visited[nr][nc] = true;
-                        continue;
-                    }
-
-                    if (map[nr][nc] == '#') {
-                        flag = false;
-                        break;
-                    }
-
-                    queue.add(new int[]{nr, nc});
-                    visited[nr][nc] = true;
-                }
-            }
-
-            while (!deleteQueue.isEmpty() && flag) {
-                int[] current = deleteQueue.poll();
-                int curRow = current[0];
-                int curCol = current[1];
-
-                map[curRow][curCol] = '0';
+                int cost = map[nr][nc] == '0' ? 0 : 1;
+                cnt[nr][nc] = Math.min(cnt[nr][nc], cnt[curRow][curCol] + cost);
+                if (map[nr][nc] == '0') deque.addFirst(new int[]{nr, nc});
+                else deque.addLast(new int[]{nr, nc});
+                visited[nr][nc] = true;
             }
         }
 
-        System.out.println(cnt);
+        System.out.println(cnt[r2][c2]);
     }
 }
